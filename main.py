4ca8227d173ea2a069f6833a032e74862a7d6e53
emiilmagdy <dremilmagdy@gmail.com>
2025-04-28 17:12:25 +0300

    initialization

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+
+if __name__ == "__main__":
+    main()
